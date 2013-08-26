a1f307c8d3802cfff8b2547f80cdfaa2e4a72d8a
Hartmut Goebel <h.goebel@crazy-compilers.com>
2013-08-26 20:49:55 +0200

    Add mini-packages and script for 'nspkg2' namespace packages.

--- tests/eggs4testing/make.sh
+++ tests/eggs4testing/make.sh
@@ -13,7 +13,7 @@
 
 export PYTHONDONTWRITEBYTECODE=1
 
-rm -rf build/ dist/ *.egg-info
+rm -rf build/ dist/ *.egg* */*.egg* */build/ */dist/
 
 python setup-zipped.py bdist_egg
 # nedd to clean up build-dir, otherwise stuff from `zipped_egg`
@@ -22,25 +22,41 @@ rm -rf build/
 python setup-unzipped.py bdist_egg
 rm -rf build/
 
+PKGS="nspkg2-aaa nspkg2-bbb nspkg2-ccc"
+
+distdir="$PWD"/dist
+
+for pkg in $PKGS ; do
+    cd $pkg
+    python setup.py sdist --dist-dir "$distdir"
+    python setup.py bdist_egg --dist-dir "$distdir"
+    cd -
+done
+
 # setup a virtualenv for testing
 virtualenv venv --distribute
 . venv/bin/activate
-easy_install --zip-ok dist/zipped_egg*.egg
-easy_install --always-unzip dist/unzipped_egg*.egg
-cp ../import/test_eggs*.py venv
+easy_install --zip-ok "$distdir"/zipped_egg*.egg
+easy_install --always-unzip "$distdir"/dist/unzipped_egg*.egg
+easy_install "$distdir"/nspkg2_*.egg
+cp ../import/test_{eggs,nspkg2}*.py venv
 
 # see if the unpackaged test-case still works
 cd venv
 python test_eggs1.py
 python test_eggs2.py
+python test_nspkg2.py
 cd ..
 
 cd venv
 rm -rfv ../../import/zipped.egg ../../import/unzipped.egg
 mv -v lib/python2.7/site-packages/zipped_egg-*.egg ../../import/zipped.egg
 mv -v lib/python2.7/site-packages/unzipped_egg-*.egg ../../import/unzipped.egg
+mv -v lib/python2.7/site-packages/nspkg2_aaa-*.egg/ ../../import/nspkg2_aaa.egg
+mv -v lib/python2.7/site-packages/nspkg2_bbb-*.egg/ ../../import/nspkg2_bbb.egg
+mv -v lib/python2.7/site-packages/nspkg2_ccc-*.egg/ ../../import/nspkg2_ccc.egg
 cd ..
 
 deactivate
 
-rm -rf build/ dist/ venv *.egg-info
+rm -rf build/ dist/ *.egg* */*.egg* */build/ */dist/ venv
---
+++ tests/eggs4testing/nspkg2-ccc/setup.py
@@ -0,0 +1,12 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='nspkg2-ccc',
+    version='0.1',
+    description='A test package for name-spaces',
+    zip_safe=False,
+    packages=find_packages(),
+    namespace_packages = ['nspkg2']
+    )
