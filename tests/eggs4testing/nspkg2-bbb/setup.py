f766694aaca5fb14015587a68ac3003eb8348323
Hartmut Goebel <h.goebel@crazy-compilers.com>
2013-08-27 00:46:00 +0200

    Change one of the nspkg2 eggs into a zipped one.
    We need to test namespaces in zipped eggs, too.

--- tests/eggs4testing/nspkg2-bbb/setup.py
+++ tests/eggs4testing/nspkg2-bbb/setup.py
@@ -6,7 +6,7 @@ setup(
     name='nspkg2-bbb',
     version='0.1',
     description='A test package for name-spaces',
-    zip_safe=False,
+    zip_safe=True,
     packages=find_packages(),
     namespace_packages = ['nspkg2']
     )
